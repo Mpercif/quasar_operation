bff1f1ad2359b9a194d1c890f66a2f5162dcf844
Mpercif <miguelperezc2017@gmail.com>
2023-12-17 11:11:30 -0500

    Update Resources logic

--- quasarservice/app/api/resources/ImperialFleet.py
+++ quasarservice/app/api/resources/ImperialFleet.py
@@ -1,20 +1,33 @@
 import json
 
+from flasgger import swag_from
 from flask_restful import Resource
 from jsonschema.exceptions import ValidationError
 from jsonschema.validators import validate
 
+from quasarservice.app.api.controllers.ImperialFleetController import (
+    ImperialFleetController,
+)
 from quasarservice.app.api.controllers.JsonSchemaController import JsonSchemaController
+from quasarservice.app.exceptions.errors import SatelliteValidationNotSuccess
+from quasarservice.default_config import APIDOCS_PATH
 from quasarservice.helpers.responseHelper import json_response
 
 
 class ImperialFleet(Resource):
-
-    def post(self, satellite_triangulation):
+    @swag_from(APIDOCS_PATH + "/v1.1/business_loan/loan_list.yml")
+    def post(self, fleet_information):
         try:
-            data = json.loads(satellite_triangulation)
+            data = json.loads(fleet_information)
             self.__validate_params(data)
+            satellites = ImperialFleetController().setup_satellite_configuration(
+                data["satellites"]
+            )
+            response = ImperialFleetController().triangulate_position_float(satellites)
 
+            return response
+        except SatelliteValidationNotSuccess as e:
+            return json_response({"message": e}, e.code)
         except ValidationError as e:
             return json_response({"message": e}, 400)
         except ValueError as e:
--- quasarservice/app/api/resources/Satellite.py
+++ quasarservice/app/api/resources/Satellite.py
@@ -1,20 +1,31 @@
 import json
 
+from flask import request
 from flask_restful import Resource
 from jsonschema.exceptions import ValidationError
 from jsonschema.validators import validate
 
+from quasarservice.app.api.controllers.ImperialFleetController import ImperialFleetController
 from quasarservice.app.api.controllers.JsonSchemaController import JsonSchemaController
+from quasarservice.app.api.controllers.SatelliteController import SatelliteController
+from quasarservice.app.exceptions.errors import (
+    SatelliteValidationNotSuccess,
+    SatelliteNotFound,
+)
 from quasarservice.helpers.responseHelper import json_response
 
 
 class Satellite(Resource):
-
-    def post(self, satellite_info):
+    def post(self, satellite_name):
         try:
-            data = json.loads(satellite_info)
+            data = json.loads(request.args.get("data"))
             self.__validate_params(data)
+            data.update({"name": satellite_name})
+            SatelliteController().update_satellite_data(data)
 
+            return json_response({"message": "Satellite data have been updated"})
+        except SatelliteNotFound as e:
+            return json_response({"message": e.message}, e.code)
         except ValidationError as e:
             return json_response({"message": e}, 400)
         except ValueError as e:
@@ -23,7 +34,22 @@ class Satellite(Resource):
             return json_response({"message": e}, 500)
 
     def get(self, satellite_name):
-        pass
+        try:
+            data = json.loads(request.args.get("data"))
+            self.__validate_params(data)
+            data.update({"name": satellite_name})
+            satellites = SatelliteController().get_missing_satellites(data)
+            response = ImperialFleetController().triangulate_position_float(satellites)
+
+            return response
+        except SatelliteValidationNotSuccess as e:
+            return json_response({"message": e}, e.code)
+        except ValidationError as e:
+            return json_response({"message": e}, 400)
+        except ValueError as e:
+            return json_response({"message": e}, 400)
+        except Exception as e:
+            return json_response({"message": e}, 500)
 
     def __validate_params(self, data):
         json_schema_controller = JsonSchemaController()
