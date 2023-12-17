37e5acea05084185321ab8d54aa1945862df0b3e
Mpercif <miguelperezc2017@gmail.com>
2023-12-17 13:48:23 -0500

    Update default_config and add requirements

--- quasarservice/default_config.py
+++ quasarservice/default_config.py
@@ -3,3 +3,8 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 APIDOCS_PATH = basedir + "/app/api/apidocs"
 JSON_SCHEMA_PATH = basedir + "/app/api/json_schemas"
+SECRET_KEY = "fdsjkEERkjkW1jk$5Jk%#12dsfdsadjk32"
+SQLALCHEMY_DATABASE_URI = (
+    os.environ.get("QS_DATABASE_URL") or "postgresql://postgres:testenv@localhost/mercadolibre"
+)
+SQLALCHEMY_TRACK_MODIFICATIONS = False
