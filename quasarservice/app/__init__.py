7ac0dc53b8de247a54f91e536563ee8b6133e6d9
Mpercif <miguelperezc2017@gmail.com>
2023-12-17 13:44:04 -0500

    Add config heroku

--- quasarservice/app/__init__.py
+++ quasarservice/app/__init__.py
@@ -4,7 +4,4 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config.from_object("quasarservice.default_config")
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:testenv@localhost/mercadolibre"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
 db = SQLAlchemy(app)
