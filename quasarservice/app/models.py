0e7a226c763c3320019d1d2291c0ec937b1fbab0
Mpercif <miguelperezc2017@gmail.com>
2023-12-17 11:12:34 -0500

    Update models

--- quasarservice/app/models.py
+++ quasarservice/app/models.py
@@ -7,8 +7,8 @@ class ImperialFleet(db.Model):
     __tablename__ = "imperial_fleet"
 
     id = db.Column(db.Integer, Sequence("imperial_fleet_id_seq"), primary_key=True)
-    position = db.Column(db.String(20), unique=True, nullable=False)
-    message = db.Column(db.String(100), unique=True, nullable=False)
+    position = db.Column(db.String(100), unique=True, nullable=False)
+    message = db.Column(db.String(300), unique=True, nullable=False)
 
     def __init__(self, position, message):
         self.position = position
@@ -18,14 +18,13 @@ class Satellite(db.Model):
     __tablename__ = "satellite"
 
     id = db.Column(db.Integer, Sequence("satellite_id_seq"), primary_key=True)
-    position = db.Column(db.String(20), unique=True, nullable=False)
-    distance = db.Column(db.Integer(10), unique=True, nullable=False)
-    message = db.Column(db.String(100), unique=True, nullable=False)
+    name = db.Column(db.String(20), unique=True, nullable=False)
+    position = db.Column(db.String(20), nullable=False)
+    distance = db.Column(db.Float(), nullable=True)
+    message = db.Column(db.String(300), nullable=True)
 
-    def __init__(self, position, distance, message):
+    def __init__(self, name, position, distance, message):
+        self.name = name
         self.position = position
         self.distance = distance
         self.message = message
-
-db.create_all()
-
