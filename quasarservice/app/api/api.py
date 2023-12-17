9b35528e37b3980031930f40776fbb0454ba3e8b
Mpercif <miguelperezc2017@gmail.com>
2023-12-17 11:13:00 -0500

    Update api endpoints

--- quasarservice/app/api/api.py
+++ quasarservice/app/api/api.py
@@ -7,7 +7,7 @@ from quasarservice.app.api.resources.Satellite import Satellite
 api = Api(app)
 
 api.add_resource(
-    ImperialFleet, "/topsecret/<string:satellite_triangulation>", endpoint="Satellite-triangulation"
+    ImperialFleet, "/topsecret/<string:fleet_information>", endpoint="Satellite-triangulation"
 )
 
 api.add_resource(Satellite, "/topsecret_split/<string:satellite_name>", endpoint="Satellite-info")
