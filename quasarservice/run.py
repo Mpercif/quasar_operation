fb882d71ecdfa66dba112f6fe30e4bddbf1f097a
Mpercif <miguelperezc2017@gmail.com>
2023-12-17 11:13:35 -0500

    Update run file

--- quasarservice/run.py
+++ quasarservice/run.py
@@ -1,3 +1,4 @@
+from flasgger import Swagger
 from flask_migrate import Migrate, MigrateCommand
 from flask_restful import Api
 from flask_script import Manager, Server, Shell
@@ -5,7 +6,14 @@ from flask_script import Manager, Server, Shell
 from quasarservice.app import app, db
 from quasarservice.app.api.resources.ImperialFleet import ImperialFleet
 from quasarservice.app.api.resources.Satellite import Satellite
+from quasarservice.default_config import APIDOCS_PATH
 
+app.config["SWAGGER"] = {
+    "description": "Quasar Operation Service API Docs",
+    "version": "0.1.1",
+}
+
+swagger = Swagger(app, template_file=APIDOCS_PATH + "/base.yml")
 migrate = Migrate(app, db)
 manager = Manager(app)
 
@@ -22,7 +30,7 @@ manager.add_command(
 api = Api(app)
 
 api.add_resource(
-    ImperialFleet, "/topsecret/<string:satellite_triangulation>", endpoint="Satellite-triangulation"
+    ImperialFleet, "/topsecret/<string:fleet_information>", endpoint="Satellite-triangulation"
 )
 
 api.add_resource(Satellite, "/topsecret_split/<string:satellite_name>", endpoint="Satellite-info")
